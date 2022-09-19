db8e5a05b636bd9f6d998ee49f846bec7c374ee0
RoiHatsune7 <janielpinzon@yahoo.es>
2022-09-18 19:52:56 -0500

    Adición de las cédulas y nombres faltantes a los arreglos correspondientes de tipo String. Ajuste de cargado de arreglo voto.

--- app/src/main/java/com/jeanj/hpa4_proyecto01/MainActivity.java
+++ app/src/main/java/com/jeanj/hpa4_proyecto01/MainActivity.java
@@ -37,7 +37,35 @@ public class MainActivity extends AppCompatActivity {
         Datos.listacedula.add("08-0986-000549");
         Datos.listacedula.add("08-0957-001827");
         Datos.listacedula.add("08-0940-001311");
-
+        Datos.listacedula.add("08-0863-001620");
+        Datos.listacedula.add("08-0964-000691");
+        Datos.listacedula.add("08-0954-001126");
+        Datos.listacedula.add("08-1039-001537");
+        Datos.listacedula.add("08-0925-001168");
+        Datos.listacedula.add("08-0940-001505");
+        Datos.listacedula.add("08-0938-000925");
+        Datos.listacedula.add("03-0743-002493");
+        Datos.listacedula.add("0E-0008-169755");
+        Datos.listacedula.add("08-0971-001811");
+        Datos.listacedula.add("08-0940-000408");
+        Datos.listacedula.add("08-0937-001640");
+        Datos.listacedula.add("08-0939-001578");
+        Datos.listacedula.add("08-0942-001406");
+        Datos.listacedula.add("06-0722-000724");
+        Datos.listacedula.add("08-0941-001215");
+        Datos.listacedula.add("08-0962-001218");
+        Datos.listacedula.add("08-0941-000924");
+        Datos.listacedula.add("08-0980-002414");
+        Datos.listacedula.add("03-0742-002055");
+        Datos.listacedula.add("08-0942-001192");
+        Datos.listacedula.add("08-0904-001189");
+        Datos.listacedula.add("08-0959-000857");
+        Datos.listacedula.add("08-0964-001554");
+        Datos.listacedula.add("08-1066-001018");
+        Datos.listacedula.add("08-1055-000701");
+        Datos.listacedula.add("08-0970-000242");
+        Datos.listacedula.add("03-0752-001461");
+        Datos.listacedula.add("03-0745-000950");
 
         Datos.listanombres.add("El admin");
         Datos.listanombres.add("ARROCHA, EDWIN");
@@ -50,9 +78,39 @@ public class MainActivity extends AppCompatActivity {
         Datos.listanombres.add("DUDLEY, MARC");
         Datos.listanombres.add("FERNANDEZ, DIEGO");
         Datos.listanombres.add("FLORES, MARÍA");
+        Datos.listanombres.add("FRAZER, DONALDO");
+        Datos.listanombres.add("GASCÓN, MIGUEL");
+        Datos.listanombres.add("GONZALEZ, ANDREW");
+        Datos.listanombres.add("HERNÁNDEZ, VALERIE");
+        Datos.listanombres.add("HO, TOMMY");
+        Datos.listanombres.add("JIMÉNEZ, JEAN");
+        Datos.listanombres.add("LOO, FELIPE");
+        Datos.listanombres.add("MCDONALD, LISSETH");
+        Datos.listanombres.add("MONAGAS, JESUS");
+        Datos.listanombres.add("MORENO, DIANA");
+        Datos.listanombres.add("MORENO, GERARDO");
+        Datos.listanombres.add("NAVARRO, JUAN");
+        Datos.listanombres.add("NAVAS, JULIO");
+        Datos.listanombres.add("PINZÓN, ANEL");
+        Datos.listanombres.add("PINZÓN, JANIEL");
+        Datos.listanombres.add("QIU, ANTONIO");
+        Datos.listanombres.add("RICORD, CARLOS");
+        Datos.listanombres.add("RIOS, DAVID");
+        Datos.listanombres.add("RODRIGUEZ, SERGIO");
+        Datos.listanombres.add("SALAS, ZULIXA");
+        Datos.listanombres.add("SAMANIEGO, LUIS");
+        Datos.listanombres.add("SÁNCHEZ, ARY");
+        Datos.listanombres.add("SANTOS, DIONISIO");
+        Datos.listanombres.add("THOMAS, BRYAN");
+        Datos.listanombres.add("TUÑÓN, CARLOS");
+        Datos.listanombres.add("VITALE, GENESIS");
+        Datos.listanombres.add("ZENG, FERNANDO");
+        Datos.listanombres.add("ZHONG, CARLOS");
+        Datos.listanombres.add("GUERRA, EIMY");
+
 
         Datos.voto.add("1");
-        for (i=0; i<10; i++)
+        for (i=0; i<39; i++)
         {
             Datos.voto.add("0");
         }
